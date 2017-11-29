e77c50b9ad4f94f654411ae74f6813637c0002ca
wuxiaolong <pro.invlong@gmail.com>
2017-11-29 15:23:12 +0800

    embed CacheCloud Platform

--- src/routes/Basic/RedisList.js
+++ src/routes/Basic/RedisList.js
@@ -1,12 +1,11 @@
 import React, { PureComponent } from 'react';
+import Iframe from 'react-iframe';
 import { connect } from 'dva';
-import { Row, Col, Card, Form, Input, Select, Button, Modal, Badge, Divider, notification } from 'antd';
-import StandardTable from '../../components/StandardTable';
+import { Row, Col, Form, Input, Select, Button } from 'antd';
 import PageHeaderLayout from '../../layouts/PageHeaderLayout';
 import styles from './RedisList.less';
 
 const { Option } = Select;
-const statusMap = ['default', 'processing', 'success', 'error'];
 const FormItem = Form.Item;
 
 @connect(state => ({
@@ -16,19 +15,14 @@ const FormItem = Form.Item;
 export default class TableList extends PureComponent {
   state = {
     expandForm: false,
-    selectedRows: [],
-    visible: false,
-    operation: '',
-    key: '',
-    operationName: '',
   };
 
-  componentDidMount() {
-    const { dispatch } = this.props;
-    dispatch({
-      type: 'redis/fetch',
-    });
-  }
+  // componentDidMount() {
+  //   const { dispatch } = this.props;
+  //   dispatch({
+  //     type: 'redis/fetch',
+  //   });
+  // }
 
   handleFormReset = () => {
     const { form, dispatch } = this.props;
@@ -42,12 +36,6 @@ export default class TableList extends PureComponent {
   toggleForm = () => {
     this.setState({
       expandForm: !this.state.expandForm,
-    });
-  }
-
-  handleSelectRows = (rows) => {
-    this.setState({
-      selectedRows: rows,
     });
   }
 
@@ -67,48 +55,6 @@ export default class TableList extends PureComponent {
     });
   }
 
-  handleShowModal = (operation, keyParam, instantName) => {
-    this.setState({
-      operation: operation === 1 ? '启动' : '停止',
-      visible: true,
-      key: keyParam,
-      operationName: instantName,
-    });
-  }
-  handleOk = () => {
-    const { dispatch } = this.props;
-    this.setState({
-      visible: false,
-    });
-    dispatch({
-      type: 'redis/changeDbStatus',
-      payload: {
-        id: this.state.key,
-        type: this.state.operation,
-      },
-    });
-  }
-  handleCancel = () => {
-    this.setState({
-      visible: false,
-    });
-  }
-  handleOperationDone = (operationData) => {
-    const { dispatch } = this.props;
-    if (operationData.result === '1') {
-      dispatch({
-        type: 'redis/fetch',
-      });
-    } else {
-      notification.error({
-        message: '操作发生错误',
-        description: operationData.msg,
-      });
-    }
-    dispatch({
-      type: 'redis/resetOperation',
-    });
-  }
 
   renderSimpleForm() {
     const { getFieldDecorator } = this.props.form;
@@ -148,87 +94,19 @@ export default class TableList extends PureComponent {
   }
 
   render() {
-    const { redis: { loading, data, operationDone, operationData } } = this.props;
-    const { selectedRows } = this.state;
-    const status = ['关闭', '运行中', '已上线', '异常'];
-    const columns = [
-      {
-        title: '实例名',
-        dataIndex: 'instantName',
-      },
-      {
-        title: 'Redis版本',
-        dataIndex: 'version',
-      },
-      {
-        title: '状态',
-        dataIndex: 'status',
-        filters: [
-          {
-            text: status[0],
-            value: 0,
-          },
-          {
-            text: status[1],
-            value: 1,
-          },
-          {
-            text: status[2],
-            value: 2,
-          },
-          {
-            text: status[3],
-            value: 3,
-          },
-        ],
-        render(val) {
-          return <Badge status={statusMap[val]} text={status[val]} />;
-        },
-      },
-      {
-        title: '说明',
-        dataIndex: 'desc',
-      },
-      {
-        title: '操作',
-        render: (text, record) => (
-          <div>
-            <a onClick={() => this.handleShowModal(1, record.id, record.instantName)}>启动</a>
-            <Divider type="vertical" />
-            <a onClick={() => this.handleShowModal(0, record.id, record.instantName)}>停止</a>
-          </div>
-        ),
-      },
-    ];
-
     return (
-      <PageHeaderLayout title="查询表格">
-        {
-          operationDone &&
-          this.handleOperationDone(operationData)
-        }
-        <Card bordered={false}>
-          <div className={styles.tableList}>
-            <div className={styles.tableListForm}>
-              {this.renderForm()}
-            </div>
-            <StandardTable
-              selectedRows={selectedRows}
-              loading={loading}
-              data={data}
-              columns={columns}
-              onSelectRow={this.handleSelectRows}
-            />
-          </div>
-        </Card>
-        <Modal
-          title="确认操作"
-          visible={this.state.visible}
-          onOk={this.handleOk}
-          onCancel={this.handleCancel}
-        >
-          <p>你确定要对【{this.state.operationName}】执行【{this.state.operation}】操作吗？</p>
-        </Modal>
+      <PageHeaderLayout title="CacheCloud">
+        <div className={styles.tableList}>
+          <Iframe
+            url="http://10.0.0.95:8585/admin/app/list.do"
+            width="100%"
+            height="800px"
+            id="myId"
+            display="initial"
+            position="relative"
+            allowFullScreen
+          />
+        </div>
       </PageHeaderLayout>
     );
   }
